8bf4cd032455eb28039ffba5b6f6837567d0a394
Nico Vogel <nico-vogel97@outlook.de>
2023-12-05 20:28:16 +0000

    concat md path with base path

--- src/reference.ts
+++ src/reference.ts
@@ -13,14 +13,31 @@ addMarkdownSlides(test);
 function addMarkdownSlides(path: string): void {
   const slides = getSlidesDiv();
 
+  const baseUrl = getBaseURL();
+  const fullPath = concatenatePaths(baseUrl, path);
+
   const next = document.createElement('section');
-  next.setAttribute('data-markdown', path);
+  next.setAttribute('data-markdown', fullPath);
   next.setAttribute('data-separator', '^-----\\n');
   next.setAttribute('data-separator-vertical', '^---\\n');
   next.setAttribute('data-separator-notes', '^{{Notes}}\\n');
   next.setAttribute('data-charset', 'iso-8859-15');
 
   slides.appendChild(next);
+}
+
+function concatenatePaths(first: string, second: string): string {
+  // Trim any trailing slashes from the first path and leading slashes from the second path
+  const trimmedFirst = first.replace(/\/+$/, '');
+  const trimmedSecond = second.replace(/^\/+/, '');
+
+  // Concatenate the two paths, ensuring a single slash between them
+  return `${trimmedFirst}/${trimmedSecond}`;
+}
+
+function getBaseURL(): string {
+  const base = document.querySelector('base');
+  return base ? base.getAttribute('href') || '' : '';
 }
 
 function getSlidesDiv() {
