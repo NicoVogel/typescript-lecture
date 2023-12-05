3c999757ae7aa1e7188d094de3b072c2e8fbbe78
Nico Vogel <nico-vogel97@outlook.de>
2023-12-05 19:15:57 +0000

    introduce theme switch for code

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,6 @@ import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 
 import 'reveal.js/dist/reset.css';
 import 'reveal.js/dist/reveal.css';
-// choose theme from here: https://highlightjs.org/examples
-import 'highlight.js/styles/github-dark.css';
 
 import './reference';
 import './theme/theme.scss';
@@ -20,3 +18,40 @@ Reveal.initialize({
   hash: true,
   plugins: [Markdown, Highlight, Notes],
 });
+
+// choose theme from here: https://highlightjs.org/examples
+import lightThemeUrl from 'highlight.js/styles/github.css?url';
+import darkThemeUrl from 'highlight.js/styles/github-dark.css?url';
+
+function loadStyleSheet(url: string): void {
+  const existingLink = document.querySelector('link[data-theme="highlightjs"]');
+  if (existingLink) {
+    existingLink.setAttribute('href', url);
+  } else {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    link.dataset.theme = 'highlightjs';
+    document.head.appendChild(link);
+  }
+}
+
+function applyTheme(): void {
+  const isDarkMode = document.body.classList.contains('dark');
+  loadStyleSheet(isDarkMode ? darkThemeUrl : lightThemeUrl);
+}
+
+// Observe changes to the class attribute of the body tag
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+      applyTheme();
+    }
+  });
+});
+
+// Start observing
+observer.observe(document.body, {attributes: true});
+
+// Apply initial theme when the page is loaded
+document.addEventListener('DOMContentLoaded', applyTheme);
