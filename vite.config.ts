4c398962288815baa4f9790b498e9c9820a69707
Nico Vogel <nico-vogel97@outlook.de>
2023-12-23 12:59:17 +0000

    improve dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import {defineConfig} from 'vite';
 import markdownImagePlugin from './src/features/vite-markdown-image-importer-plugin';
+import Checker from 'vite-plugin-checker';
 
-export default defineConfig({
-  plugins: [markdownImagePlugin()],
-  base: './',
-  build: {
-    minify: false,
-  },
+export default defineConfig(({command}) => {
+  return {
+    plugins: [
+      command === 'build' ? Checker({typescript: true}) : undefined,
+      markdownImagePlugin(),
+    ],
+    base: './',
+    build: {
+      minify: false,
+      rollupOptions: {
+        input: {
+          app: './index.html',
+        },
+      },
+    },
+    server: {
+      open: './index.dev.html',
+    },
+  };
 });
