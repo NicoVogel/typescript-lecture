accb05eb19ac0f402d3b1a14b1b8b199d2ed842c
Nico Vogel <nico-vogel97@outlook.de>
2023-12-23 13:35:10 +0000

    introduce typesafe events

---
+++ src/features/typesafe-events/index.ts
@@ -0,0 +1,14 @@
+type GetDetails<T extends keyof HTMLElementEventMap> =
+  HTMLElementEventMap[T] extends CustomEvent<infer U> ? U : never;
+
+export function createEvent<T extends keyof HTMLElementEventMap>(
+  eventName: T,
+  detail: GetDetails<T>
+) {
+  // eslint-disable-next-line no-restricted-syntax
+  return new CustomEvent(eventName, {
+    detail,
+    bubbles: true,
+    composed: true,
+  });
+}
