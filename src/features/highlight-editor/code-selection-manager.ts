4fd7648dfe5480d4c7d80546b7a5500fc2c73215
Nico Vogel <nico-vogel97@outlook.de>
2023-12-24 11:12:32 +0000

    extract business logic into separate file

--- src/features/highlight-editor/code-selection-manager.ts
+++ src/features/highlight-editor/code-selection-manager.ts
@@ -8,6 +8,7 @@ import {
 } from 'lit';
 import {customElement, state} from 'lit/decorators.js';
 import type {SelectionDetail} from './code-editor-wrapper';
+import {Selection, Selections} from './selection';
 
 export type ListEntry = EntryGroup | {type: 'separator'};
 
@@ -25,8 +26,8 @@ type ColumnEntry = {
 
 @customElement('code-selection-manager')
 export class CodeSelectionManager extends LitElement {
-  @state() private selections: ListEntry[] = [];
-  @state() private latestSelection?: ListEntry;
+  private selections = new Selections();
+  @state() private latestSelection?: Selection;
 
   static get styles(): CSSResultGroup {
     return css`
@@ -131,14 +132,14 @@ export class CodeSelectionManager extends LitElement {
   }
 
   handleCodeSelection(event: CustomEvent<SelectionDetail>): void {
-    this.latestSelection = parseSelection(event.detail);
+    this.latestSelection = new Selection(event.detail);
   }
 
   render(): TemplateResult {
     return html`
       ${this.renderHeader()}
       <ul>
-        ${this.selections.map((selection, index) =>
+        ${[...this.selections].map((selection, index) =>
           this.renderListElement(selection, index)
         )}
       </ul>
@@ -148,7 +149,7 @@ export class CodeSelectionManager extends LitElement {
   private renderHeader() {
     return html` <div>
       <div>
-        <pre>${getCombinedSelectionText(this.selections)}</pre>
+        <pre>${this.selections.toString()}</pre>
         <button @click="${this.copyToClipboard}">
           <reveal-icon icon="clipboard"></reveal-icon>
         </button>
@@ -156,28 +157,26 @@ export class CodeSelectionManager extends LitElement {
       <div>
         <pre>
           ${this.latestSelection
-            ? convertListEntryToHighlightSelectionText(this.latestSelection)
+            ? this.latestSelection.toString()
             : 'Nothing selected yet'}
         </pre
         >
-        ${this.latestSelection
-          ? html`<button @click="${this.addSelectionToList}">
-              Add to List
-            </button>`
-          : ''}
+        <button
+          ?disabled="${this.latestSelection === undefined}"
+          @click="${this.addSelectionToList}"
+        >
+          <reveal-icon icon="plus"></reveal-icon>
+        </button>
       </div>
       <button @click="${this.addSeparatorToList}">Add Separator</button>
     </div>`;
   }
 
-  private renderListElement(selection: ListEntry, index: number) {
-    const text =
-      selection.type === 'separator'
-        ? 'Separator'
-        : convertListEntryToHighlightSelectionText(selection);
+  private renderListElement(selection: Selection, index: number) {
+    const text = selection.isSeparator ? 'Separator' : selection.toString();
     return html`<li class="list-item">
       <pre class="item-text">${text}</pre>
-      ${selection.type === 'columns'
+      ${selection.isColumn
         ? html` <comp-slider
             class="slider"
             ?isActive=${selection.fullLine}
@@ -209,46 +208,31 @@ export class CodeSelectionManager extends LitElement {
 
   private addSelectionToList(): void {
     if (!this.latestSelection) return;
-    this.selections = [...this.selections, this.latestSelection];
+    this.selections.addSelection(this.latestSelection);
     this.latestSelection = undefined;
   }
 
   private addSeparatorToList(): void {
-    this.selections = [...this.selections, {type: 'separator'}];
+    this.selections.addSeparator();
+    this.requestUpdate('selections');
   }
 
   private deleteSelection(index: number): void {
-    this.selections = [
-      ...this.selections.slice(0, index),
-      ...this.selections.slice(index + 1),
-    ];
+    this.selections.deleteSelection(index);
+    this.requestUpdate('selections');
   }
 
   private moveSelection(index: number, direction: 'up' | 'down'): void {
-    if (
-      (direction === 'up' && index > 0) ||
-      (direction === 'down' && index < this.selections.length - 1)
-    ) {
-      const newIndex = direction === 'up' ? index - 1 : index + 1;
-      const newSelections = [...this.selections];
-      [newSelections[index], newSelections[newIndex]] = [
-        newSelections[newIndex],
-        newSelections[index],
-      ];
-      this.selections = newSelections;
-    }
+    this.selections.moveSelection(index, direction);
+    this.requestUpdate('selections');
   }
 
   private toggleFullLine(index: number): void {
-    const selection = this.selections[index];
-    if (selection && 'columns' in selection) {
-      selection.fullLine = !selection.fullLine;
-      this.requestUpdate();
-    }
+    this.selections.toggleFullLine(index);
   }
 
   private copyToClipboard(): void {
-    const textToCopy = getCombinedSelectionText(this.selections);
+    const textToCopy = this.selections.toString();
     navigator.clipboard
       .writeText(textToCopy)
       .then(() => {
@@ -258,206 +242,4 @@ export class CodeSelectionManager extends LitElement {
         console.error('Failed to copy text: ', err);
       });
   }
-}
-
-function parseSelection(selection: SelectionDetail): ListEntry {
-  const {startPosition, endPosition} = selection;
-  if (startPosition.row === endPosition.row) {
-    return {
-      type: 'columns',
-      start: startPosition.row,
-      columns: [{from: startPosition.position, to: endPosition.position}],
-      fullLine: false,
-    };
-  } else {
-    return {
-      type: 'range',
-      start: startPosition.row,
-      end: endPosition.row,
-    };
-  }
-}
-
-function convertListEntryToHighlightSelectionText(
-  selection: ListEntry
-): string {
-  switch (selection.type) {
-    case 'range':
-      return `${selection.start}-${selection.end}`;
-    case 'columns':
-      if (selection.fullLine) {
-        return `${selection.start}`;
-      }
-      return `${selection.start}(${selection.columns
-        .map(column => `${column.from}-${column.to}`)
-        .join(',')})`;
-    case 'single':
-      return `${selection.start}`;
-    case 'separator':
-      return '';
-    default:
-      return '';
-  }
-}
-
-function getCombinedSelectionText(selections: ListEntry[]): string {
-  const groupedSelections = groupSelections(selections);
-  return groupedSelections
-    .map(group => {
-      const mergedGroup = mergeGroup(group);
-      return mergedGroup
-        .map(convertListEntryToHighlightSelectionText)
-        .join(',');
-    })
-    .join('|');
-}
-
-function groupSelections(selections: ListEntry[]): EntryGroup[][] {
-  const groups: EntryGroup[][] = [];
-  let currentGroup: EntryGroup[] = [];
-
-  selections.forEach(selection => {
-    if (selection.type === 'separator') {
-      groups.push(currentGroup);
-      currentGroup = [];
-    } else {
-      currentGroup.push(selection);
-    }
-  });
-
-  if (currentGroup.length > 0) {
-    groups.push(currentGroup);
-  }
-
-  return groups;
-}
-
-function mergeGroup(group: EntryGroup[]): EntryGroup[] {
-  const {selectedColumns, selectedLines} = decomposeGroup(group);
-  const ranges = reconstructRanges(selectedLines);
-
-  const rangeInColumn = [...selectedColumns.entries()].map(
-    ([line, columns]) => {
-      const column: ColumnEntry = {
-        type: 'columns',
-        start: line,
-        columns: reconstructColumns(columns),
-        fullLine: false,
-      };
-      return column;
-    }
-  );
-
-  return [...ranges, ...rangeInColumn].sort((a, b) => a.start - b.start);
-}
-
-function decomposeGroup(group: ListEntry[]) {
-  const selectedLines = new Set<number>();
-  const selectedColumns = new Map<number, Set<number>>();
-
-  const addRange = (from: number, to: number, range: Set<number>) => {
-    for (let i = from; i <= to; i++) {
-      range.add(i);
-    }
-  };
-
-  group.forEach(selection => {
-    switch (selection.type) {
-      case 'range':
-        addRange(selection.start, selection.end, selectedLines);
-        break;
-      case 'columns':
-        if (selection.fullLine) {
-          selectedLines.add(selection.start);
-          break;
-        }
-        const columns =
-          selectedColumns.get(selection.start) ?? new Set<number>();
-        selection.columns.forEach(column => {
-          // column selections are 1-based, but we need 0-based for the range
-          addRange(column.from, column.to - 1, columns);
-        });
-        selectedColumns.set(selection.start, columns);
-        break;
-      case 'single':
-        selectedLines.add(selection.start);
-        break;
-      case 'separator':
-        break;
-    }
-  });
-
-  // if the full line is already selected, remove the column selection
-  selectedLines.forEach(line => {
-    selectedColumns.delete(line);
-  });
-
-  return {selectedLines, selectedColumns};
-}
-
-function reconstructRanges(selectedLines: Set<number>) {
-  const addEntry = (
-    range: {start: number; end: number},
-    group: EntryGroup[]
-  ) => {
-    const newEntry =
-      range.start === range.end
-        ? ({
-            type: 'single',
-            start: range.start,
-          } as const)
-        : ({
-            type: 'range',
-            start: range.start,
-            end: range.end,
-          } as const);
-    group.push(newEntry);
-  };
-  const mergedGroup: EntryGroup[] = [];
-  let currentRange: {start: number; end: number} | undefined;
-
-  [...selectedLines]
-    .sort((a, b) => a - b)
-    .forEach(line => {
-      if (currentRange && line === currentRange.end + 1) {
-        currentRange.end = line;
-      } else {
-        if (currentRange) {
-          addEntry(currentRange, mergedGroup);
-        }
-        currentRange = {start: line, end: line};
-      }
-    });
-  if (currentRange) {
-    addEntry(currentRange, mergedGroup);
-  }
-  return mergedGroup;
-}
-
-function reconstructColumns(
-  selectedLines: Set<number>
-): ColumnEntry['columns'] {
-  const mergedGroup: ReturnType<typeof reconstructColumns> = [];
-  let currentRange: {from: number; to: number} | undefined;
-
-  [...selectedLines]
-    .sort((a, b) => a - b)
-    .forEach(line => {
-      if (currentRange && line === currentRange.to + 1) {
-        currentRange.to = line;
-      } else {
-        if (currentRange) {
-          mergedGroup.push(currentRange);
-        }
-        currentRange = {from: line, to: line};
-      }
-    });
-  if (currentRange) {
-    mergedGroup.push(currentRange);
-  }
-  return mergedGroup.map(column => {
-    // revert the base conversion from decomposeGroup
-    column.to++;
-    return column;
-  });
 }
---
+++ src/features/highlight-editor/selection.ts
@@ -0,0 +1,306 @@
+import {SelectionDetail} from './code-editor-wrapper';
+
+export type ListEntry = EntryGroup | {type: 'separator'};
+
+type EntryGroup =
+  | ColumnEntry
+  | {type: 'range'; start: number; end: number}
+  | {type: 'single'; start: number};
+
+type ColumnEntry = {
+  type: 'columns';
+  start: number;
+  columns: {from: number; to: number}[];
+  fullLine: boolean;
+};
+
+export class Selection {
+  private entry: ListEntry;
+
+  get isSeparator() {
+    return this.entry.type === 'separator';
+  }
+
+  get isColumn() {
+    return this.entry.type === 'columns';
+  }
+
+  get fullLine() {
+    if (this.entry.type !== 'columns') {
+      return false;
+    }
+    return this.entry.fullLine;
+  }
+
+  public constructor(details?: SelectionDetail | EntryGroup) {
+    if (!details) {
+      this.entry = {type: 'separator'};
+      return;
+    }
+    if ('startPosition' in details) {
+      this.entry = this.parseSelection(details);
+      return;
+    }
+    this.entry = details;
+  }
+
+  toggleFillLine(): void {
+    if (this.entry && 'columns' in this.entry) {
+      this.entry.fullLine = !this.entry.fullLine;
+    }
+  }
+
+  toString() {
+    switch (this.entry.type) {
+      case 'range':
+        return `${this.entry.start}-${this.entry.end}`;
+      case 'columns':
+        if (this.entry.fullLine) {
+          return `${this.entry.start}`;
+        }
+        return `${this.entry.start}(${this.entry.columns
+          .map(column => `${column.from}-${column.to}`)
+          .join(',')})`;
+      case 'single':
+        return `${this.entry.start}`;
+      case 'separator':
+        return '';
+      default:
+        return '';
+    }
+  }
+
+  decompose(
+    selectedLines: Set<number>,
+    selectedColumns: Map<number, Set<number>>
+  ) {
+    switch (this.entry.type) {
+      case 'range':
+        this.addRange(this.entry.start, this.entry.end, selectedLines);
+        break;
+      case 'columns':
+        if (this.entry.fullLine) {
+          selectedLines.add(this.entry.start);
+          break;
+        }
+        const columns =
+          selectedColumns.get(this.entry.start) ?? new Set<number>();
+        this.entry.columns.forEach(column => {
+          // column selections are 1-based, but we need 0-based for the range
+          this.addRange(column.from, column.to - 1, columns);
+        });
+        selectedColumns.set(this.entry.start, columns);
+        break;
+      case 'single':
+        selectedLines.add(this.entry.start);
+        break;
+      case 'separator':
+        break;
+    }
+  }
+
+  compare(other: Selection): number {
+    if (this.entry.type === 'separator' || other.entry.type === 'separator') {
+      return 0;
+    }
+    return this.entry.start - other.entry.start;
+  }
+
+  private addRange(from: number, to: number, range: Set<number>) {
+    for (let i = from; i <= to; i++) {
+      range.add(i);
+    }
+  }
+
+  private parseSelection(selection: SelectionDetail): ListEntry {
+    const {startPosition, endPosition} = selection;
+    if (startPosition.row === endPosition.row) {
+      return {
+        type: 'columns',
+        start: startPosition.row,
+        columns: [{from: startPosition.position, to: endPosition.position}],
+        fullLine: false,
+      };
+    } else {
+      return {
+        type: 'range',
+        start: startPosition.row,
+        end: endPosition.row,
+      };
+    }
+  }
+}
+
+export class Selections {
+  private selections: Selection[] = [];
+
+  addSelection(selection: Selection): void {
+    this.selections = [...this.selections, selection];
+  }
+
+  addSeparator(): void {
+    this.selections = [...this.selections, new Selection()];
+  }
+
+  deleteSelection(index: number): void {
+    this.selections = [
+      ...this.selections.slice(0, index),
+      ...this.selections.slice(index + 1),
+    ];
+  }
+
+  moveSelection(index: number, direction: 'up' | 'down'): void {
+    if (
+      (direction === 'up' && index > 0) ||
+      (direction === 'down' && index < this.selections.length - 1)
+    ) {
+      const newIndex = direction === 'up' ? index - 1 : index + 1;
+      const newSelections = [...this.selections];
+      [newSelections[index], newSelections[newIndex]] = [
+        newSelections[newIndex],
+        newSelections[index],
+      ];
+      this.selections = newSelections;
+    }
+  }
+
+  toggleFullLine(index: number): void {
+    this.selections[index].toggleFillLine();
+  }
+
+  [Symbol.iterator](): Iterator<Selection> {
+    return this.selections[Symbol.iterator]();
+  }
+
+  toString(): string {
+    const groupedSelections = groupSelections(this.selections);
+    return groupedSelections
+      .map(group => {
+        const mergedGroup = mergeGroup(group);
+        return mergedGroup.map(selection => selection.toString()).join(',');
+      })
+      .join('|');
+  }
+}
+
+function groupSelections(selections: Selection[]): Selection[][] {
+  const groups: Selection[][] = [];
+  let currentGroup: Selection[] = [];
+
+  selections.forEach(selection => {
+    if (selection.isSeparator) {
+      groups.push(currentGroup);
+      currentGroup = [];
+    } else {
+      currentGroup.push(selection);
+    }
+  });
+
+  if (currentGroup.length > 0) {
+    groups.push(currentGroup);
+  }
+
+  return groups;
+}
+
+function mergeGroup(group: Selection[]): Selection[] {
+  const {selectedColumns, selectedLines} = decomposeGroup(group);
+  const ranges = reconstructRanges(selectedLines);
+
+  const rangeInColumn = [...selectedColumns.entries()].map(
+    ([line, columns]) => {
+      const column: ColumnEntry = {
+        type: 'columns',
+        start: line,
+        columns: reconstructColumns(columns),
+        fullLine: false,
+      };
+      return new Selection(column);
+    }
+  );
+
+  return [...ranges, ...rangeInColumn].sort((a, b) => a.compare(b));
+}
+
+function decomposeGroup(group: Selection[]) {
+  const selectedLines = new Set<number>();
+  const selectedColumns = new Map<number, Set<number>>();
+
+  group.forEach(selection => {
+    selection.decompose(selectedLines, selectedColumns);
+  });
+
+  // if the full line is already selected, remove the column selection
+  selectedLines.forEach(line => {
+    selectedColumns.delete(line);
+  });
+
+  return {selectedLines, selectedColumns};
+}
+
+function reconstructRanges(selectedLines: Set<number>) {
+  const addEntry = (
+    range: {start: number; end: number},
+    group: Selection[]
+  ) => {
+    const newEntry =
+      range.start === range.end
+        ? ({
+            type: 'single',
+            start: range.start,
+          } as const)
+        : ({
+            type: 'range',
+            start: range.start,
+            end: range.end,
+          } as const);
+    group.push(new Selection(newEntry));
+  };
+  const mergedGroup: Selection[] = [];
+  let currentRange: {start: number; end: number} | undefined;
+
+  [...selectedLines]
+    .sort((a, b) => a - b)
+    .forEach(line => {
+      if (currentRange && line === currentRange.end + 1) {
+        currentRange.end = line;
+      } else {
+        if (currentRange) {
+          addEntry(currentRange, mergedGroup);
+        }
+        currentRange = {start: line, end: line};
+      }
+    });
+  if (currentRange) {
+    addEntry(currentRange, mergedGroup);
+  }
+  return mergedGroup;
+}
+
+function reconstructColumns(
+  selectedLines: Set<number>
+): ColumnEntry['columns'] {
+  const mergedGroup: ReturnType<typeof reconstructColumns> = [];
+  let currentRange: {from: number; to: number} | undefined;
+
+  [...selectedLines]
+    .sort((a, b) => a - b)
+    .forEach(line => {
+      if (currentRange && line === currentRange.to + 1) {
+        currentRange.to = line;
+      } else {
+        if (currentRange) {
+          mergedGroup.push(currentRange);
+        }
+        currentRange = {from: line, to: line};
+      }
+    });
+  if (currentRange) {
+    mergedGroup.push(currentRange);
+  }
+  return mergedGroup.map(column => {
+    // revert the base conversion from decomposeGroup
+    column.to++;
+    return column;
+  });
+}
